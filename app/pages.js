a2b22e949cc22326df6984ec1618487c0c8e164e
huyenthuong166 <stu725105167@hnue.edu.vn>
2025-10-01 14:36:42 +0700

    Create pages.js

---
+++ app/pages.js
@@ -0,0 +1,9 @@
+// app/page.js
+export default function Page() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Bookstore Online</h1>
+      <p>Backend / frontend is running.</p>
+    </main>
+  )
+}
